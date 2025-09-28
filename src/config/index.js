66995f80ead6301836bb694650d5f37ff1d86d60
Srijan Kharel <mrsrjn224@gmail.com>
2025-09-28 23:25:11 +0545

    undo

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const config = {
     serverHost: process.env.SERVER_HOST,
     port: process.env.PORT,
     supabaseUrl: process.env.SUPABASE_URL,
-    supabaseKey: process.env.SUPABASE_KEY,
+    supabaseKey: process.env.SERVICE_ROLE,
 };
 
 console.log('config:', config);
