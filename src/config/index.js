6a6be83dcef23d881efbd7e85f8fdb5cea30c8fd
Srijan Kharel <mrsrjn224@gmail.com>
2025-09-28 23:22:29 +0545

    base

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const config = {
     serverHost: process.env.SERVER_HOST,
     port: process.env.PORT,
     supabaseUrl: process.env.SUPABASE_URL,
-    supabaseKey: process.env.SERVICE_ROLE,
+    supabaseKey: process.env.SUPABASE_KEY,
 };
 
 console.log('config:', config);
